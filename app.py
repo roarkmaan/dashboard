7dd183ada93cd000bcf72d413ecd281e44afb5f9
HR <anon@anon.com>
2013-12-10 22:46:45 -0500

    Small flask API up and running. Next step is to integrate a database, then merge to AWS.

--- app.py
+++ app.py
@@ -1,15 +1,59 @@
 #!bin/python
-from flask import Flask
+from flask import Flask, jsonify, abort, make_response, request
 
 app = Flask(__name__)
 
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return "Welcome!"
 
-@app.route('/<var>')
-def indexP(var):
-    return "Hello, World! You've reached " + var
+@app.route('/<o>')
+def index_(o):
+    return "Welcome!"
 
+@app.route('/todo/api/v1.0/tasks', methods = ['GET'])
+def get_tasks():
+    return jsonify( { 'tasks': tasks } )
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['GET'])
+def get_task(task_id):
+    task = [i for i in tasks if task_id == i['id']]
+    if len(task) == 0:
+        abort(404)
+    return jsonify( { 'task': task[0] } )
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Not found' } ), 404)
+
+@app.route('/todo/api/v1.0/tasks', methods = ['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify( { 'task': task } ), 201
+
+#Run it
 if __name__ == '__main__':
     app.run(debug = True)
